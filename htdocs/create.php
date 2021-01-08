4b00dab856395a15920704a946a47c0a95361b88
dabin0513 <63287630+dabin0513@users.noreply.github.com>
2021-01-08 23:23:53 +0900

    2021.01.08-write, update, delete
    글 생성, 수정, 삭제

---
+++ htdocs/create.php
@@ -0,0 +1,18 @@
+<?php
+require_once('lib/print.php');
+require('view/top.php');
+?>
+    <a href="create.php">create</a>
+    <h2>
+    <form action="create_process.php" method="post">
+      <p>
+        <input type="text" name="title" placeholder="Title">
+      </p>
+      <p>
+        <textarea name="description" placeholder="Description"></textarea>
+      </p>
+        <input type="submit">
+    </form>
+<?php
+require('view/bottom.php');
+?>
